0e5d45dabdefc60c4d9e8ebab9ec916695af82e3
Satish Kori <kori.satish@ingka.ikea.com>
2025-09-02 21:37:41 +0200

    fixed a error

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-import pkg from "../package.json" assert { type: "json" };
+import pkg from "../package.json";
 
 // Import tool functions
 import { calculate } from "./tools/calculator.js";
